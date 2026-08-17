4a7d4dffc49cfe18bfd5393b17fcd184d7cde2a7
Freddy Angel <freddy_andres@hotmail.com>
2026-08-17 02:55:35 -0400

    feat: configure postgres datasource

--- src/main/java/LinkShortener/entity/Link.java
+++ src/main/java/LinkShortener/entity/Link.java
@@ -4,14 +4,17 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "links")
 public class Link {
 
     @Id
     @GeneratedValue()
     private Long id;
+    @Column(nullable = false)
     private String originalUrl;
-    @Column(length = 6, unique = true)
+    @Column(length = 6, unique = true, nullable = false)
     private String shortCode;
 }
